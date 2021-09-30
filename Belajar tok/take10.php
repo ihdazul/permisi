646c2f9dba624a3507a4eafc73e8b9bbc4b881a5
ihdazul <ihdazul123@gmail.com>
2021-10-01 02:14:39 +0700

    take10.php

---
+++ Belajar tok/take10.php
@@ -0,0 +1,15 @@
+<title>Coba coba</title>
+<?php
+
+$a = 5;
+$b = 10;
+
+	echo "{$a} + {$b} = " . ($a + $b) . " <br>";
+	echo "{$a} - {$b} = " . ($a - $b) . " <br>";
+	echo "{$a} * {$b} = " . ($a * $b) . " <br>";
+	echo "{$a} / {$b} = " . ($a / $b) . " <br>";
+	echo "{$a} % {$b} = " . ($a % $b) . " <br>";
+	echo "{$a} ** {$b} = " . ($a ** $b) . " <br>";
+	echo "-a = ". (-$a) . " <br>";
+
+?>
